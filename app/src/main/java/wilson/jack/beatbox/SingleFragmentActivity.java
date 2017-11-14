4a7b2ee3346d481d5c444c50a8653419e2f732c1
JackEWilson <jack.wilson.east@gmail.com>
2017-11-14 13:34:27 -0800

    listing 20.5

--- app/src/main/java/wilson/jack/beatbox/SingleFragmentActivity.java
+++ app/src/main/java/wilson/jack/beatbox/SingleFragmentActivity.java
@@ -18,7 +18,6 @@ public abstract class SingleFragmentActivity  extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_fragment);
         setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
