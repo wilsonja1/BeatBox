d54a09afc06986ff0b08e2a7a50ddbbd1d83abfc
JackEWilson <jack.wilson.east@gmail.com>
2017-11-14 13:43:19 -0800

    listing 20.8

--- app/src/main/java/wilson/jack/beatbox/BeatBoxFragment.java
+++ app/src/main/java/wilson/jack/beatbox/BeatBoxFragment.java
@@ -1,11 +1,28 @@
 package wilson.jack.beatbox;
 
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import wilson.jack.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return binding.getRoot();
     }
 }
