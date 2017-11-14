879e429e83059af51a4b71dd5b2d062e7f31c3fe
JackEWilson <jack.wilson.east@gmail.com>
2017-11-14 13:23:28 -0800

    basic fragments, copied over singlefragment activity stuff from criminal intent. listing 20.2

---
+++ app/src/main/java/wilson/jack/beatbox/BeatBoxFragment.java
@@ -0,0 +1,11 @@
+package wilson.jack.beatbox;
+
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
